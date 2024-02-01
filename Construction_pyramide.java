b782625b9db97977da7334790017db64a8408690
Mohamed Ali Farhat <100702507+MohamedAliFathat@users.noreply.github.com>
2024-02-01 14:04:15 +0000

    solution

---
+++ Construction_pyramide.java
@@ -0,0 +1,35 @@
+
+
+class Main {
+    public static void main(String[] args)
+     {
+     
+     
+  int tot=0; 
+
+
+
+for (int j = 17; j >= 1; j = j - 2)
+
+{
+
+
+tot=tot+(j*j*j);
+
+
+}
+
+
+System.out.println(tot);
+
+
+
+
+
+
+
+}
+
+
+
+}
